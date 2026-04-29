c428e7ec84eaa1efbc0df63a33fb5eb0990a45e7
cwoland <tkachenkoa2003@gmail.com>
2026-04-29 07:50:55 +0300

    Update vite.config.ts

--- Kino/vite.config.ts
+++ Kino/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/Kino',
+  base: '/Portfolio/Kino/dist',
   plugins: [
     vue(),
     vueDevTools(),
